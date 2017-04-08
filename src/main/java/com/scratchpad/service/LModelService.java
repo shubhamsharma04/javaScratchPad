8a82d52bef9990dee2566e1202926ad2b3aa67a5
opensam <opensam4@gmail.com>
2017-04-08 10:18:07 -0400

    LModel for gradiance

--- src/main/java/com/scratchpad/service/LModelService.java
+++ src/main/java/com/scratchpad/service/LModelService.java
@@ -3,10 +3,10 @@ package com.scratchpad.service;
 public class LModelService {
 	
 	static int [] input = new int[]{11,18,17,15,9,5,12,19,22,25};
-	static int [] ouput = new int[]{6,18,17,15,9,5,12,19,22,25};
+	static int [] ouput = new int[]{6,8,10,4,9,6,3,5,2,10};
 	
 	public static void printModelsOutput() {
-		float[] w1 = new float[] { 0.30f, 0.50f };
+		double[] w1 = new double[] { 0.30d, 0.50d };
 		double sum = 0d;
 		for (int i = 0; i < input.length; i++) {
 			float err = (float) Math.pow(ouput[i] - (w1[0] + (w1[1] * input[i])), 2);
@@ -14,7 +14,7 @@ public class LModelService {
 		}
 		System.out.println("Total err1 : " + sum);
 		sum = 0d;
-		w1 = new float[] { 5.75f, 0.04f };
+		w1 = new double[] { 5.75d, 0.04d };
 		for (int i = 0; i < input.length; i++) {
 			float err = (float) Math.pow(ouput[i] - (w1[0] + (w1[1] * input[i])), 2);
 			sum += err;
@@ -22,7 +22,7 @@ public class LModelService {
 		System.out.println("Total err2 : " + sum);
 
 		sum = 0d;
-		w1 = new float[] { 3.20f, 0.20f };
+		w1 = new double[] { 3.20d, 0.20d };
 		for (int i = 0; i < input.length; i++) {
 			double err = Math.pow(ouput[i] - (w1[0] + (w1[1] * input[i])), 2);
 			sum += err;
@@ -30,16 +30,30 @@ public class LModelService {
 		System.out.println("Total err3 : " + sum);
 
 		sum = 0d;
-		w1 = new float[] { 8.75f, -0.50f, 0.02f };
+		w1 = new double[] { 8.75d, -0.50d, 0.02d };
 		for (int i = 0; i < input.length; i++) {
-			float err = (float) Math.pow(ouput[i] - (w1[0] + (w1[1] * input[i]) + (w1[2] * input[i] * input[i])), 2);
+			double err = (float) Math.pow(ouput[i] - (w1[0] + (w1[1] * input[i]) + (w1[2] * input[i] * input[i])), 2);
 			sum += err;
 		}
 		System.out.println("Total err4 : " + sum);
-		
-		
+		double [] logSize = new double[]{2.18d,1.09d,-.37d,0d,-0.42d,0.09d,-0.48d,0.62d,0.57d,0.44d};
+		double [] dis = new double[]{0.01d,18.30d,3.00d,4.10d,9.00d,7.20d,10.00d,2.70d,2.80d,0.01d};
+		w1 = new double[] { 1.05d, -0.52d, 0.85d };
+		for(int i =0;i<logSize.length;i++){
+			double err = (double) (w1[0]+w1[1]*logSize[i]+w1[2]*dis[i]);
+			err = sigmoid(err);
+			System.out.printf(i+" : %f",err);//w1[0]+w1[1]*logSize[i]+w1[2]*dis[i]);
+			String result = Double.compare(err, 0.5d)>=0?"1":"0";
+			System.out.println(i+" : "+result);
+			
+		}
 
 
+	}
+
+	private static double sigmoid(double err) {
+		
+		return 1/(1+Math.exp(-err));
 	}
 	
 	
