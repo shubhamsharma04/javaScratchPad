5301ac18ae37a92e55871830e86fa3ba1541c012
opensam <opensam4@gmail.com>
2017-04-14 16:08:22 -0400

    Removed unused import

--- src/main/java/com/scratchpad/dataformat/MsgObject.java
+++ src/main/java/com/scratchpad/dataformat/MsgObject.java
@@ -1,6 +1,5 @@
 package com.scratchpad.dataformat;
 
-import java.util.Comparator;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
