43567dc508f2af6b0ad783be1971da7a0f84a322
opensam <opensam4@gmail.com>
2017-05-03 22:20:55 -0400

    Compare Strings

---
+++ src/main/java/com/scratchpad/service/ServicePartThree.java
@@ -0,0 +1,12 @@
+package com.scratchpad.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServicePartThree {
+
+	public void compareStrings() {
+		System.out.println("cj9quwxICXrGuamJ20TCqXyACMlnbR1u".compareTo("c25ddd596aa7c81fa12378fa725f706d54325d12"));
+	}
+
+}
