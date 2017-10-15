cd12fa53d05a54c10b0a77fded91e5b81e529dbc
opensam <opensam4@gmail.com>
2017-10-15 10:51:03 -0400

    CombinationsSum

---
+++ src/main/java/com/opensam/ft/CombinationSum.java
@@ -0,0 +1,21 @@
+package com.opensam.ft;
+
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CombinationSum implements Comparator<String>{
+
+	public static void main(String[] args) {
+		StringBuilder str = new StringBuilder();
+		System.out.println('1' - '2');
+        List<String> input = new LinkedList<String>();
+	}
+
+	@Override
+	public int compare(String o1, String o2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
